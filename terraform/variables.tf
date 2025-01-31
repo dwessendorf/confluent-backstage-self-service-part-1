8a632782b43db3bbd4597a06f9c9133330b43fe0
Daniel Wessendorf <dan@bossmail.de>
2025-01-31 10:04:02 +0000

    Refactor GitHub Actions workflow to use environment variables for Confluent Cloud API key and secret; add descriptions to Terraform variables for clarity.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,29 +1,35 @@
 variable "confluent_cloud_api_key" {
-  type      = string
-  sensitive = true
+  description = "Confluent Cloud API Key"
+  type        = string
+  sensitive   = true
 }
 
 variable "confluent_cloud_api_secret" {
-  type      = string
-  sensitive = true
+  description = "Confluent Cloud API Secret"
+  type        = string
+  sensitive   = true
 }
 
 variable "environment_name" {
-  type    = string
-  default = "backstage-env"
+  description = "Name for the Confluent environment"
+  type        = string
+  default     = "backstage-env"
 }
 
 variable "cluster_name" {
-  type    = string
-  default = "backstage-cluster"
+  description = "Name for the Kafka cluster"
+  type        = string
+  default     = "backstage-cluster"
 }
 
 variable "cloud_provider" {
-  type    = string
-  default = "GCP"
+  description = "Cloud provider (AWS, GCP, AZURE)"
+  type        = string
+  default     = "GCP"
 }
 
 variable "region" {
-  type    = string
-  default = "europe-west3"
+  description = "Cloud region for the cluster"
+  type        = string
+  default     = "europe-west3"
 }
